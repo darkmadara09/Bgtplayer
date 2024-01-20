1a18e68649d77aaa77e12f8781ff773913ab7a1e
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-20 17:25:57 +0530

    Update database

---
+++ Love/database/__init__.py
@@ -0,0 +1,11 @@
+#MIT License
+#Copyright (c) 2023, ©NovaNetworks
+
+from async_pymongo import AsyncClient
+
+from Bikash.config import MONGO_DB_URI
+
+DBNAME = "Bikash"
+
+mongo = AsyncClient(MONGO_DB_URI)
+dbname = mongo[DBNAME]
